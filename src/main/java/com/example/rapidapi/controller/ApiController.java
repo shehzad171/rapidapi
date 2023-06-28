15a2bacec08e4a92caf64684563f94ab44f88865
unknown <ibrahim.shehzad7@gmail.com>
2023-06-28 11:03:33 +0530

    second commit

--- src/main/java/com/example/rapidapi/controller/ApiController.java
+++ src/main/java/com/example/rapidapi/controller/ApiController.java
@@ -21,4 +21,9 @@ public class ApiController {
     public String getAlluArjunImages(@PathVariable String query) {
         return rapidApiService.makeRapidApiRequest(query);
     }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Rapid API";
+    }
 }
